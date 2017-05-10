ab7d81e556b252172c9dd8a9002aea4c6ca93ca2
Igor Kislyuk <igorkislyuk@icloud.com>
2017-05-11 00:57:43 +0300

    Add. Example with template page

--- server.js
+++ server.js
@@ -4,29 +4,39 @@ const fs = require('fs');
 const path = require('path');
 
 function sendError(err, response) {
+    console.log(err);
     response.writeHead(err, {"Content-Type": 'text/plain'});
     response.write(err.toString() + '. Resource not found!');
     response.end();
 }
 
 const server = http.createServer(function (request, response) {
-    const filename = './public/index.html';
-
-    fs.exists(filename, function (exist) {
-        if (exist) {
-            fs.readFile(filename, function (err, data) {
-                if (err) {
-                    sendError(err.code, response);
-                }
-
-                response.writeHead(200, {"Content-Type": mime.lookup(path.basename(filename))});
-                response.end(data);
-            })
-        } else {
-            sendError(404, response);
-        }
-    });
+    getTitles(response);
 });
+
+function getTitles(response) {
+    const filename = './resources/titles.json';
+    fs.readFile(filename, function (err, data) {
+        if (err) return sendError(404, response);
+
+        getTemplate(JSON.parse(data.toString()), response);
+    });
+}
+
+function getTemplate(titles, response) {
+    const filename = './public/template.html';
+    fs.readFile(filename, function (err, data) {
+        if (err) return sendError(404, response);
+
+        formatHtml(titles, data.toString(), response);
+    });
+}
+
+function formatHtml(titles, template, response) {
+    const html = template.replace('%', titles.join('</li><li>'));
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end(html);
+}
 
 const port = 3000;
 server.listen(port, function () {
