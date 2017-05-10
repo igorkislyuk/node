e6ecce7a864409b7d7643cd30afdfc2604baff74
Igor Kislyuk <igorkislyuk@icloud.com>
2017-05-11 02:11:59 +0300

    Add. Script watching local files, moves files from `watch` to `done` folder and converts to lower case

--- server.js
+++ server.js
@@ -1,58 +1,43 @@
-const net = require('net');
-const events = require('events');
+const events = require('events')
+    , util = require('util')
+    , fs = require('fs');
 
-var channel = new events.EventEmitter();
-channel.clients = {};
-channel.subscribtions = {};
-channel.setMaxListeners(50);
+const watchDir = './watch'
+    , processedDir = './done';
 
-
-channel.on('join', function (id, client) {
-    this.clients[id] = client;
-    const welcome = "Welcome!\n" +
-        'Guests online: ' + this.listeners('broadcast').length;
-    client.write(welcome);
-    this.subscribtions[id] = function (senderId, message) {
-        if (id !== senderId) {
-            this.clients[id].write(message);
-        }
-    };
-
-    this.on('broadcast', this.subscribtions[id]);
-});
-
-channel.on('leave', function (id) {
-    channel.removeListener('broadcast', this.subscribtions[id]);
-
-    channel.emit('broadcast', id, id + " has left the chat.\n");
-});
-
-channel.on('shutdown', function () {
-    channel.emit('broadcast', '', 'Chat has shut down.\n');
-    channel.removeAllListeners('broadcast');
-});
-
-const server = net.createServer(function (client) {
-    const id = client.remoteAddress + ':' + client.remotePort;
-    channel.emit('join', id, client);
-
-    client.on('data', function(data) {
-        data = data.toString();
-        processCommand(data, client, channel);
-        channel.emit('broadcast', id, data);
-    });
-
-    client.on('close', function () {
-        channel.emit('leave', id);
-    });
-});
-
-function processCommand(command, client, channel) {
-    if (command === 'end\r\n') {
-        client.emit('end');
-    } else if (command === 'shutdown\r\n') {
-        channel.emit('shutdown');
-    }
+function Watcher(watchDir, processedDir) {
+    this.watchDir = watchDir;
+    this.processedDir = processedDir;
 }
+util.inherits(Watcher, events.EventEmitter);
 
-server.listen(8888);
+Watcher.prototype.watch = function () {
+    const watcher = this;
+    fs.readdir(this.watchDir, function (err, files) {
+        if (err) throw err;
+
+        for (var index in files) {
+            // process per file in folder
+            watcher.emit('process', files[index]);
+        }
+    });
+};
+
+Watcher.prototype.start = function () {
+    var watcher = this;
+    fs.watchFile(watchDir, function () {
+        watcher.watch();
+    });
+};
+
+const watcher = new Watcher(watchDir, processedDir);
+watcher.on('process', function process(file) {
+    const watchFile = this.watchDir + '/' + file;
+    const processedFile = this.processedDir + '/' + file.toLowerCase();
+
+    fs.rename(watchFile, processedFile, function (err) {
+        if (err) throw err;
+    });
+});
+
+watcher.start();
