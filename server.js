c035e95875274b014f94a39f922c3f96c990ced2
Igor Kislyuk <igorkislyuk@icloud.com>
2017-05-31 20:50:02 +0300

    Add. Update files for non-parallel programming task

--- server.js
+++ server.js
@@ -1,43 +1,32 @@
-const events = require('events')
-    , util = require('util')
-    , fs = require('fs');
+// setTimeout(function () {
+//     console.log('I execute first.');
+//     setTimeout(function () {
+//         console.log('I execute next.');
+//         setTimeout(function () {
+//             console.log('I execute last');
+//         }, 100);
+//     }, 500);
+// }, 1000);
 
-const watchDir = './watch'
-    , processedDir = './done';
+const flow = require('nimble');
 
-function Watcher(watchDir, processedDir) {
-    this.watchDir = watchDir;
-    this.processedDir = processedDir;
-}
-util.inherits(Watcher, events.EventEmitter);
-
-Watcher.prototype.watch = function () {
-    const watcher = this;
-    fs.readdir(this.watchDir, function (err, files) {
-        if (err) throw err;
-
-        for (var index in files) {
-            // process per file in folder
-            watcher.emit('process', files[index]);
-        }
-    });
-};
-
-Watcher.prototype.start = function () {
-    var watcher = this;
-    fs.watchFile(watchDir, function () {
-        watcher.watch();
-    });
-};
-
-const watcher = new Watcher(watchDir, processedDir);
-watcher.on('process', function process(file) {
-    const watchFile = this.watchDir + '/' + file;
-    const processedFile = this.processedDir + '/' + file.toLowerCase();
-
-    fs.rename(watchFile, processedFile, function (err) {
-        if (err) throw err;
-    });
-});
-
-watcher.start();
+flow.series([
+    function (callback) {
+        setTimeout(function () {
+            console.log('I execute first');
+            callback();
+        }, 1000);
+    },
+    function (callback) {
+        setTimeout(function () {
+            console.log('I execute next');
+            callback();
+        }, 500);
+    },
+    function (callback) {
+        setTimeout(function () {
+            console.log('I execute last');
+            callback();
+        }, 100);
+    }
+]);
