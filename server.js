a4011e18078e1ad242cbfdd2e55b12909eac668b
Igor Kislyuk <igorkislyuk@icloud.com>
2017-05-31 22:32:25 +0300

    Add. To-do server on GET/POST/PUT/DELETE

--- server.js
+++ server.js
@@ -12,16 +12,65 @@ const server = http.createServer(function (req, res) {
                 item += chunk;
             });
 
-            req.on('end',function () {
+            req.on('end', function () {
                 items.push(item);
                 res.end('Ok\n');
             });
             break;
         case 'GET':
-            items.forEach(function (item, i) {
-                res.write(i + ')' + item + '\n');
-            });
-            res.end();
+            var body = items.map(function (item, i) {
+                return i + ') ' + item;
+            }).join('\n');
+            res.setHeader('Content-Length', Buffer.byteLength(body));
+            res.setHeader('Content-Type', 'text/plain; charset="utf-8"');
+            res.end(body);
+            break;
+        case 'DELETE':
+            const path = url.parse(req.url).pathname;
+            const i = parseInt(path.slice(1), 10);
+
+            if (isNaN(i)) {
+                res.statusCode = 400;
+                res.end('Invalid item id');
+            } else if (!items[i]) {
+                res.statusCode = 404;
+                res.end('Item not found!');
+            } else {
+                items.splice(i, 1);
+                res.end('Ok\n');
+            }
+            break;
+        case 'PUT':
+            const pathComponent = url.parse(req.url).pathname;
+            const index = parseInt(pathComponent.slice(1));
+            console.log(index);
+
+            if (isNaN(index)) {
+                res.statusCode = 400;
+                res.end('Invalid item id');
+            } else if (!items[index]) {
+                res.statusCode = 404;
+                res.end('Item not found!');
+            } else {
+
+                req.setEncoding('utf8');
+                var localItem = '';
+
+                req.on('data', function (chunk) {
+                    localItem += chunk;
+                });
+
+                req.on('end', function () {
+                    console.log('Complete parsing!');
+                    console.log(localItem);
+                    const old = items[index];
+                    items[index] = localItem;
+                    res.setHeader('Content-Type', 'text/plain; charset="utf-8"');
+                    res.end('Done. Replaced `' + old + '` with `'
+                        + items[index] + '`');
+                });
+            }
+
             break;
     }
 });
