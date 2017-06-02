70fea219a59f98789994033e80da7afa3843001c
Igor Kislyuk <igorkislyuk@icloud.com>
2017-06-02 22:17:24 +0300

    Add. Simple Todo server using form

--- server.js
+++ server.js
@@ -2,35 +2,71 @@ const http = require('http');
 const parse = require('url').parse;
 const join = require('path').join;
 const fs = require('fs');
+const qs = require('querystring');
 
-const server = http.createServer(function (req, res) {
-    const url = parse(req.url);
-    const path = join(root, url.pathname);
+var items = [];
 
-    fs.stat(path, function (err, stat) {
-        if (err) {
-            if ('ENOENT' == err.code) {
-                res.statusCode = 404;
-                res.end('Not Found!');
-            } else {
-                internalError(res);
-            }
-        } else {
-            res.setHeader('Content-Length', stat.size);
-            const stream = fs.createReadStream(path);
-            stream.pipe(res);
-
-            stream.on('error', function (err) {
-                internalError(res);
-            })
-        }
-    });
-});
-
-function internalError(res) {
-    res.statusCode = 500;
-    res.end('Internal server error!');
+function show(res) {
+    const html = '<html><head><title>Todo List</title></head>' +
+        '<body>' +
+        '<h1>Todo List</h1>' +
+        '<ul>' +
+            items.map(function (item) {
+                return '<li>' +  item + '</li>'
+            }).join('') +
+        '</ul>' +
+        '<form method="post" action="/">' +
+        '<p><input type="text" name="item"/></p>' +
+        '<p><input type="submit" value="Add Item" /></p>' +
+        '</form>' +
+        '</body>' +
+        '</html>';
+    res.setHeader('Content-Length', Buffer.byteLength(html));
+    res.setHeader('Content-Type', 'text/html');
+    res.end(html);
 }
 
-server.listen(3000);
+function notFound(res) {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not Found');
+}
 
+function badRequest(res) {
+    res.statusCode = 400;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Bad Request');
+}
+
+function add(req, res) {
+    var body = '';
+
+    req.setEncoding('utf-8');
+    req.on('data', function (chunk) {
+        body += chunk;
+    });
+    req.on('end', function () {
+        const obj = qs.parse(body);
+        items.push(obj.item);
+        show(res);
+    });
+}
+
+const server = http.createServer(function (req, res) {
+    if ('/' == req.url) {
+        switch (req.method) {
+            case'GET':
+                show(res);
+                break;
+            case'POST':
+                add(req, res);
+                break;
+            default:
+                badRequest(res);
+        }
+    } else {
+        notFound(res);
+    }
+});
+
+server.listen(3000);
