1e05c8cc280d7e7e8c25c9936e1bb0ee7d8753ba
Igor Kislyuk <igorkislyuk@icloud.com>
2017-06-02 21:41:38 +0300

    Add. Simple static server

--- server.js
+++ server.js
@@ -1,78 +1,31 @@
 const http = require('http');
-const url = require('url');
+const parse = require('url').parse;
+const join = require('path').join;
+const fs = require('fs');
 
-var items = [];
+var root = __dirname;
+console.log(root);
 
 const server = http.createServer(function (req, res) {
-    switch (req.method) {
-        case 'POST':
-            var item = '';
-            req.setEncoding('utf8');
-            req.on('data', function (chunk) {
-                item += chunk;
-            });
+    const url = parse(req.url);
+    const path = join(root, url.pathname);
 
-            req.on('end', function () {
-                items.push(item);
-                res.end('Ok\n');
-            });
-            break;
-        case 'GET':
-            var body = items.map(function (item, i) {
-                return i + ') ' + item;
-            }).join('\n');
-            res.setHeader('Content-Length', Buffer.byteLength(body));
-            res.setHeader('Content-Type', 'text/plain; charset="utf-8"');
-            res.end(body);
-            break;
-        case 'DELETE':
-            const path = url.parse(req.url).pathname;
-            const i = parseInt(path.slice(1), 10);
+    console.log(path);
 
-            if (isNaN(i)) {
-                res.statusCode = 400;
-                res.end('Invalid item id');
-            } else if (!items[i]) {
-                res.statusCode = 404;
-                res.end('Item not found!');
-            } else {
-                items.splice(i, 1);
-                res.end('Ok\n');
-            }
-            break;
-        case 'PUT':
-            const pathComponent = url.parse(req.url).pathname;
-            const index = parseInt(pathComponent.slice(1));
-            console.log(index);
+    const stream = fs.createReadStream(path);
 
-            if (isNaN(index)) {
-                res.statusCode = 400;
-                res.end('Invalid item id');
-            } else if (!items[index]) {
-                res.statusCode = 404;
-                res.end('Item not found!');
-            } else {
+    stream.on('data', function (chunk) {
+        res.write(chunk);
+    });
 
-                req.setEncoding('utf8');
-                var localItem = '';
-
-                req.on('data', function (chunk) {
-                    localItem += chunk;
-                });
-
-                req.on('end', function () {
-                    console.log('Complete parsing!');
-                    console.log(localItem);
-                    const old = items[index];
-                    items[index] = localItem;
-                    res.setHeader('Content-Type', 'text/plain; charset="utf-8"');
-                    res.end('Done. Replaced `' + old + '` with `'
-                        + items[index] + '`');
-                });
-            }
-
-            break;
-    }
+    stream.on('end', function () {
+        res.end();
+    });
+    
+    stream.on('error', function (error) {
+        console.log(error);
+    })
 });
 
 server.listen(3000);
+
