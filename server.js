38921afcfccce78f0a37c60877142f38be976cda
Igor Kislyuk <igorkislyuk@icloud.com>
2017-06-02 21:59:17 +0300

    Add. Error Handling

--- server.js
+++ server.js
@@ -4,12 +4,33 @@ const join = require('path').join;
 const fs = require('fs');
 
 const server = http.createServer(function (req, res) {
-    req.setEncoding('utf-8');
+    const url = parse(req.url);
+    const path = join(root, url.pathname);
 
-    const writeStream = fs.createWriteStream('./req.txt');
-    req.pipe(writeStream);
-    res.end();
+    fs.stat(path, function (err, stat) {
+        if (err) {
+            if ('ENOENT' == err.code) {
+                res.statusCode = 404;
+                res.end('Not Found!');
+            } else {
+                internalError(res);
+            }
+        } else {
+            res.setHeader('Content-Length', stat.size);
+            const stream = fs.createReadStream(path);
+            stream.pipe(res);
+
+            stream.on('error', function (err) {
+                internalError(res);
+            })
+        }
+    });
 });
+
+function internalError(res) {
+    res.statusCode = 500;
+    res.end('Internal server error!');
+}
 
 server.listen(3000);
 
