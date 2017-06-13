918be09d3e71f295d6d5f329c61372526f0a5b6c
Igor Kislyuk <igorkislyuk@icloud.com>
2017-06-14 00:58:15 +0300

    Add. Mongoose

--- server.js
+++ server.js
@@ -3,66 +3,38 @@ const http = require('http');
 const mongodb = require('mongodb');
 const ObjectID = require('mongodb').ObjectID;
 
-const server = new mongodb.Server('127.0.0.1', 27017, {});
+const mongoose = require('mongoose');
+const db = mongoose.connect('mongodb://localhost/tasks');
 
-const client = new mongodb.Db('mydatabase', server, {w: 1});
+const Schema = mongoose.Schema;
 
-client.open(function (err) {
-    if (err) {
-        throw err;
-    }
-
-    client.collection('test_insert', function (err, collection) {
-        if (err) {
-            throw err;
-        }
-
-        console.log('We are now able to perform changes.');
-
-
-        // collection.insert(
-        //     {
-        //         "title": "I like cake",
-        //         "body": "It is quite good."
-        //     },
-        //     {safe: true},
-        //     function (err, documents) {
-        //         if (err) {
-        //             throw err;
-        //         }
-        //
-        //         console.log('Document ID is: ' + documents.insertedIds[0]);
-        //
-        //         client.close();
-        //     }
-        // );
-
-        const _id = ObjectID('59405a1401efb15d34f50a62');
-
-        collection.update(
-            {_id: _id},
-            {$set: {'title': 'I ate too much cake'}},
-            {safe: true},
-            function (err) {
-                if (err) {
-                    throw err;
-                }
-            }
-        );
-
-        collection.find({
-            "body": "It is quite good."
-        }).toArray(
-            function (err, results) {
-                if (err) {
-                    throw err;
-                }
-
-                console.log(results);
-            }
-        );
-
-
-
-    });
+const Tasks = new Schema({
+    project: String,
+    description: String
 });
+
+mongoose.model('Task', Tasks);
+
+const Task = mongoose.model('Task');
+
+// const task = new Task();
+// task.project = 'Bikeshed';
+// task.description = 'Paint the bikeshed red.';
+// task.save(function (err) {
+//     if (err) {
+//         throw err;
+//     }
+//
+//     console.log('Task saved.');
+// });
+
+
+Task.find({'project': 'Bikeshed'}, function (err, tasks) {
+    for (var i = 0; i < tasks.length; i++) {
+        console.log('ID: ' + tasks[i]._id);
+        console.log(tasks[0].description);
+    }
+});
+
+
+// mongoose.disconnect();
