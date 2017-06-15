d8495c041c049313ddf048e3110e530563664496
Igor Kislyuk <igorkislyuk@icloud.com>
2017-06-15 08:24:00 +0300

    Add. Restrict zone

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const connect = require('connect');
-
 const port = 3000;
 
 function logger(req, res, next) {
@@ -17,14 +16,54 @@ function hello(req, res) {
     res.end('Hello world!\n');
 }
 
-function adminHello(req, res, next) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.write('Hello, Admin!\n');
-    next();
+function authenticateWithDatabase(user, pass, callback) {
+    let err;
+    if (user !== 'tobi' || pass !== 'ferret') {
+        err = new Error('Unauthorized');
+    }
+    callback(err);
+}
+
+function restrict(req, res, next) {
+    const authorization = req.headers.authorization;
+
+    if (!authorization) {
+        return next(new Error('Unauthorized'));
+    }
+
+    const parts = authorization.split(' ');
+    const scheme = parts[0];
+    const auth = new Buffer(parts[1], 'base64').toString().split(':');
+    const user = auth[0];
+    const pass = auth[1];
+
+    authenticateWithDatabase(user, pass, function (err) {
+        if (err) {
+            return next(err);
+        }
+
+        next();
+    });
+
+}
+
+function admin(req, res, next) {
+    switch (req.url) {
+        case '/':
+            res.end('try /users');
+            break;
+
+        case '/users':
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(['tobi', 'loki', 'jane']));
+            break;
+
+    }
 }
 
 connect()
-    .use('/admin', adminHello)
     .use(logger)
+    .use('/admin', restrict)
+    .use('/admin', admin)
     .use(hello)
     .listen(port);
