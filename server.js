932204259688e8b330e8273503d398e33750505c
Igor Kislyuk <igorkislyuk@icloud.com>
2017-06-15 07:50:32 +0300

    Add. Simple connect application

--- server.js
+++ server.js
@@ -1,63 +1,18 @@
-const http = require('http');
+const connect = require('connect');
 
-const mongodb = require('mongodb');
-const ObjectID = require('mongodb').ObjectID;
+const port = 3000;
 
-const mongoose = require('mongoose');
-const db = mongoose.connect('mongodb://localhost/tasks');
-
-const Schema = mongoose.Schema;
-
-const Tasks = new Schema({
-    project: String,
-    description: String
-});
-
-mongoose.model('Task', Tasks);
-
-const Task = mongoose.model('Task');
-
-// const task = new Task();
-// task.project = 'Bikeshed';
-// task.description = 'Paint the bikeshed red.';
-// task.save(function (err) {
-//     if (err) {
-//         throw err;
-//     }
-//
-//     console.log('Task saved.');
-// });
-
-
-function displayElements() {
-    Task.find({'project': 'Bikeshed'}, function (err, tasks) {
-        for (var i = 0; i < tasks.length; i++) {
-            console.log('\nID: ' + tasks[i]._id);
-            console.log(tasks[0].description);
-        }
-    });
+function logger(req, res, next) {
+    console.log('%s %s', req.method, req.url);
+    next();
 }
 
-displayElements();
+function hello(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello world!');
+}
 
-Task.findById('59405f83d062ce5eff017688', function (err, task) {
-    if (err) {
-        throw err;
-    }
-
-    task.description = 'Paint the bikeshed green.';
-
-    task.save(function (err, task) {
-        console.log('New description is: ' + task.description);
-        displayElements();
-    });
-});
-
-Task.findById('59405f8ef266775f039f0f66', function (err, task) {
-    if (task !== null) {
-        task.remove();
-    }
-});
-
-
-// mongoose.disconnect();
+connect()
+    .use(logger)
+    .use(hello)
+    .listen(port);
