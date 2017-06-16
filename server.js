8672dec6310bf5da4800c301db3ab1fa4bad2564
Igor Kislyuk <igorkislyuk@icloud.com>
2017-06-16 16:32:44 +0300

    Add. Logger module

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
 const connect = require('connect');
 const port = 3000;
 
-function logger(req, res, next) {
-    console.log('%s %s', req.method, req.url);
-    next();
-}
-
 function hello(req, res) {
     if (!res.headersSent) {
         res.setHeader('Content-Type', 'text/plain');
@@ -16,54 +11,9 @@ function hello(req, res) {
     res.end('Hello world!\n');
 }
 
-function authenticateWithDatabase(user, pass, callback) {
-    let err;
-    if (user !== 'tobi' || pass !== 'ferret') {
-        err = new Error('Unauthorized');
-    }
-    callback(err);
-}
-
-function restrict(req, res, next) {
-    const authorization = req.headers.authorization;
-
-    if (!authorization) {
-        return next(new Error('Unauthorized'));
-    }
-
-    const parts = authorization.split(' ');
-    const scheme = parts[0];
-    const auth = new Buffer(parts[1], 'base64').toString().split(':');
-    const user = auth[0];
-    const pass = auth[1];
-
-    authenticateWithDatabase(user, pass, function (err) {
-        if (err) {
-            return next(err);
-        }
-
-        next();
-    });
-
-}
-
-function admin(req, res, next) {
-    switch (req.url) {
-        case '/':
-            res.end('try /users');
-            break;
-
-        case '/users':
-            res.setHeader('Content-Type', 'application/json');
-            res.end(JSON.stringify(['tobi', 'loki', 'jane']));
-            break;
-
-    }
-}
+const logger = require('./logger.js');
 
 connect()
-    .use(logger)
-    .use('/admin', restrict)
-    .use('/admin', admin)
+    .use(logger(':method:url'))
     .use(hello)
-    .listen(port);
+    .listen(3000);
