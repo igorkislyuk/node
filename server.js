2c561ca41e26da551df0b2b516e56fb833523f2b
Igor Kislyuk <igorkislyuk@icloud.com>
2017-06-17 17:01:48 +0300

    Add. Router module

--- server.js
+++ server.js
@@ -13,7 +13,25 @@ function hello(req, res) {
 
 const logger = require('./logger.js');
 
+const router = require('./middleware/router');
+
+const routes = {
+    GET: {
+        '/users': function (req, res) {
+            res.end('tobi, loki, ferret');
+        },
+        '/users/:id': function (req, res, id) {
+            res.end('user ' + id);
+        }
+    },
+    DELETE: {
+        '/user/:id': function (req, res, id) {
+            res.end('deleted user ' + id);
+        }
+    }
+};
+
 connect()
     .use(logger(':method:url'))
-    .use(hello)
+    .use(router(routes))
     .listen(3000);
