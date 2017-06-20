ba9c98840f2f656539cf79e49b37a6f538073f6d
Igor Kislyuk <igorkislyuk@icloud.com>
2017-06-20 09:50:32 +0300

    Add. Simple authorization

--- server.js
+++ server.js
@@ -1,22 +1,16 @@
-const http = require('http');
-const connect = require('connect');
-const url = require('url');
-const cookieSession = require('cookie-session');
+const basicAuth = require('connect-basic-auth');
+const express = require('express');
 
-const port = 3000;
-
-connect()
-    .use(cookieSession({
-        name: 'session',
-        keys: ['secret'],
-        maxAge: 5000 // 24 hours
-    }))
-    .use(function (req, res) {
-        req.session.views = (req.session.views || 0) + 1;
-
-        // Write response
-        res.end(req.session.views + ' views')
-    })
-    .listen(port);
+const app = express();
+app.use(basicAuth(function(credentials, req, res, next) {
+    next();
 
 
+}, 'Please enter your credentials.'));
+
+//Lets require authentication for the rest of our routes.
+app.all('*', function(req, res, next) {
+    req.requireAuthorization(req, res, next);
+});
+
+app.listen(3000);
