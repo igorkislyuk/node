d6f99e1c6c56e2a925cca5359933f629a4b4027b
Igor Kislyuk <igorkislyuk@icloud.com>
2017-06-20 08:21:22 +0300

    Add. Morgan logger

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const connect = require('connect');
 const qs = require('qs');
 const url = require('url');
+const morgan = require('morgan');
+const fs = require('fs');
 
 const port = 3000;
 
+const logStream = fs.createWriteStream('test_log.log');
+
 connect()
+    .use(morgan(':method :url :status :res[content-length] - :response-time ms', {stream: logStream}))
     .use(function (req, res, next) {
         req.query = qs.parse(url.parse(req.url).query);
         console.log(req.query);
