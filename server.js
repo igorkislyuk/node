7d9f2cd50f53aab0f44758e57d6cfb326496260b
Igor Kislyuk <igorkislyuk@icloud.com>
2017-06-21 11:21:13 +0300

    Add. Simple static server

--- server.js
+++ server.js
@@ -1,18 +1,6 @@
 const connect = require('connect');
-const cookieSession = require('cookie-session');
-const errorHandler = require('errorhandler');
+const serveStatic = require('serve-static');
 
 connect()
-    .use(cookieSession({
-        name: 'session',
-        keys: ['secret'],
-        maxAge: 24 * 60 * 60 * 1000
-    }))
-    .use(function (req, res) {
-        console.log(req.session);
-
-        asd();
-        res.end();
-    })
-    .use(errorHandler())
+    .use(serveStatic('./public/'))
     .listen(3000);
