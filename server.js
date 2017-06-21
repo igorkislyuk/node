270a047ac5606724d1f7184d8df183ac5d3dfc55
Igor Kislyuk <igorkislyuk@icloud.com>
2017-06-21 11:47:13 +0300

    Add. Serve-Index

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const connect = require('connect');
 const serveStatic = require('serve-static');
 const compression = require('compression');
+const serveIndex = require('serve-index');
 
 connect()
-    .use(compression())
-    .use(serveStatic('./public/'))
+    .use('/files', serveIndex('public', {icons: true, hidden: true}))
+    .use('/files', serveStatic('./public/', {hidden: true}))
     .listen(3000);
 
 // curl http://localhost:3000/test.js -i -H "Accept-Encoding: gzip"
