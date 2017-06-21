3785e366c0a4f189f84a2e25fad299bfc2d084bd
Igor Kislyuk <igorkislyuk@icloud.com>
2017-06-21 11:42:41 +0300

    Add. Simple encoding server

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const connect = require('connect');
 const serveStatic = require('serve-static');
+const compression = require('compression');
 
 connect()
+    .use(compression())
     .use(serveStatic('./public/'))
     .listen(3000);
+
+// curl http://localhost:3000/test.js -i -H "Accept-Encoding: gzip"
