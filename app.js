22dcaefadefc6c483fec20ebe70c3aca1cc6f9d9
Igor Kislyuk <igorkislyuk@icloud.com>
2017-06-24 14:33:12 +0300

    Fix. Small configuration

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const errorHandler = require('errorhandler');
 
 const index = require('./routes/index');
 const users = require('./routes/users');
@@ -16,9 +17,19 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+const env = process.env.NODE_ENV || 'development';
+
+if ('development' === env) {
+    app.use(logger);
+    app.use(errorHandler);
+    app.set('photos', __dirname + '/public/photos');
+} else if ('production' === env) {
+    app.set('photos', __dirname + '/public/production/photos');
+}
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -26,21 +37,21 @@ app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 app.listen(3000);
