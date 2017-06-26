1438cfe9601d2719866a4e8b9023b65e893f0611
Igor Kislyuk <igorkislyuk@icloud.com>
2017-06-26 23:14:19 +0300

    Finish. Registration process

--- routes/register.js
+++ routes/register.js
@@ -1,12 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../lib/User');
 
 router.get('/', function (req, res) {
+    res.message('test message', 'info');
     res.render('register', { title: 'Register'});
 });
 
-router.post('/', function (req, res) {
-    res.send('post test');
+router.post('/', function (req, res, next) {
+    const data = {
+        name: req.body.userName,
+        password: req.body.userPass
+    };
+
+    User.getByName(data.name, function (err, user) {
+        if (err) {
+            return next(err);
+        }
+
+        if (user.id) {
+            res.error('Username already taken!');
+            res.redirect('back');
+        } else {
+            user = new User({
+                name: data.name,
+                pass: data.password
+            });
+
+            user.save(function (err) {
+                if (err) {
+                    return next(err);
+                }
+
+                req.session.id = user.id;
+                res.redirect('/');
+            });
+        }
+    });
 });
 
 module.exports = router;
+
