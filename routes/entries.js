4f27867df44f5704dae7ae441c11b36a00253996
Igor Kislyuk <igorkislyuk@icloud.com>
2017-07-01 01:00:18 +0300

    Update. Validating

--- routes/entries.js
+++ routes/entries.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Entry = require('../lib/entry');
 const qs = require('qs');
+const validate = require('../lib/middleware/validate');
 
 module.exports = router;
 
@@ -22,7 +23,12 @@ router.get('/post', function (req, res) {
     res.render('post', {title: 'Post'});
 });
 
-router.post('/post', [requireEntryTitle, requireEntryTitleLengthAbove(4)], function (req, res, next) {
+const validators = [
+    validate.required('entry[title]'),
+    validate.lengthAbove('entry[body]', 4)
+];
+
+router.post('/post', validators, function (req, res, next) {
     const data = qs.parse(req.body).entry;
 
     const entry = new Entry({
@@ -39,27 +45,3 @@ router.post('/post', [requireEntryTitle, requireEntryTitleLengthAbove(4)], funct
         res.redirect('/');
     });
 });
-
-// function for checking
-function requireEntryTitle(req, res, next) {
-    const title = qs.parse(req.body).entry.title;
-
-    if (title) {
-        next();
-    } else {
-        res.error('Title is required.');
-        res.redirect('back');
-    }
-}
-
-function requireEntryTitleLengthAbove(len) {
-    return function (req, res, next) {
-        const title = qs.parse(req.body).entry.title;
-        if (title.length > len) {
-            next();
-        } else {
-            res.error('Title must be longer than ' + len.toString());
-            res.redirect('back');
-        }
-    }
-}
